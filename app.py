875c402f419f44e9a9e533ed68ea7fa33a25ce05
Abinesh Ragavan <abineshragavan27@gmail.com>
2023-04-20 22:12:31 +0530

    final commmit next face is aws

--- app.py
+++ app.py
@@ -1,31 +1,34 @@
-from flask import Flask,render_template,request
+from flask import Flask, render_template, request
 import pickle
 import numpy as np
 
-model=pickle.load(open('model.pkl','rb'))
+model = pickle.load(open('model.pkl', 'rb'))
 
 
-app=Flask(__name__)
+app = Flask(__name__)
+
+
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/predict',methods=['POST'])
+
+@app.route('/predict', methods=['POST'])
 def predict_placement():
-    g1=int(request.form.get('g1'))
-    g2=int(request.form.get('g2'))
-    g3=int(request.form.get('g3'))
-    g4=int(request.form.get('g4'))
-    g5=int(request.form.get('g5'))
-    g6=int(request.form.get('g6'))
-    g7=int(request.form.get('g7'))
+    g1 = int(request.form.get('g1'))
+    g2 = int(request.form.get('g2'))
+    g3 = int(request.form.get('g3'))
+    g4 = int(request.form.get('g4'))
+    g5 = int(request.form.get('g5'))
+    g6 = int(request.form.get('g6'))
+    g7 = int(request.form.get('g7'))
+    name1 = str(request.form.get('name'))
 
-    result =model.predict(np.array([g1,g2,g3,g4,g5,g6,g7]).reshape(1,7))
-    name=int(result)
-    return render_template('predict.html',value =name)
-    
+    result = model.predict(
+        np.array([g1, g2, g3, g4, g5, g6, g7]).reshape(1, 7))
+    name = [name1, int(result)]
+    return render_template('predict.html', value=name)
 
 
-
-if __name__=='__main__':
+if __name__ == '__main__':
     app.run(debug=True)
